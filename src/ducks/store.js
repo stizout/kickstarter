97026ef600c9ec1531a2e0e33fe77a9b2c04b52e
Alex Stout <Alex@Alexs-MBP.domain>
2018-10-11 13:57:29 -0700

    update

--- src/ducks/store.js
+++ src/ducks/store.js
@@ -5,7 +5,7 @@ import thunk from 'redux-thunk'
 const middleware = [thunk]
 const initialState = {};
 
-const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware)));
 
 
 export default store;
